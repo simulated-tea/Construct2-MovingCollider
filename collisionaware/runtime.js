29c2edad3dfcb627cb373a9cc358e69faa0a84d3
Simon <t-sim@gmx.net>
2015-02-07 12:48:08 +0100

    Leaving it to the framework to find shortest pushout-path
    This works really well to remove items from arbitrary shapes.
    It leads however to very strange bounce-off directions half the time.

--- collisionaware/runtime.js
+++ collisionaware/runtime.js
@@ -35,7 +35,7 @@ cr.behaviors.CollisionAware = function(runtime)
         this.stuckTracker = new simulatedTea.CollisionAware.StuckTracker(); // XXX this will break at minification
     };
 
-    var toleratedNumericError = 1;
+    var toleratedNumericError = 2;
 
     var behinstProto = behaviorProto.Instance.prototype;
 
@@ -43,11 +43,13 @@ cr.behaviors.CollisionAware = function(runtime)
     {
         this.enabled = this.properties[0];
 
-        this.elasticity = 0.1;
-        this.stuckTime = 0;
+        this.elasticity = 1;
 
+        this.externDx = 0;
+        this.externDy = 0;
         this.dx = 0;
         this.dy = 0;
+        this.ticksToPush = 0;
         this.lastX = this.inst.x;
         this.lastY = this.inst.y;
         this.medianDt = 0.016; // 60 FPS
@@ -62,8 +64,11 @@ cr.behaviors.CollisionAware = function(runtime)
     {
         return {
             "enabled": this.enabled,
+            "externDx": this.externDx,
+            "externDy": this.externDy,
             "dx": this.dx,
             "dy": this.dy,
+            "ticksToPush": this.ticksToPush,
             "lastX": this.lastX,
             "lastY": this.lastY,
             "medianDt": this.medianDt,
@@ -74,48 +79,58 @@ cr.behaviors.CollisionAware = function(runtime)
     behinstProto.loadFromJSON = function (o)
     {
         this.enabled = o["enabled"];
+        this.externDx = o["externDx"];
+        this.externDy = o["externDy"];
         this.dx = o["dx"];
         this.dy = o["dy"];
+        this.ticksToPush = o["ticksToPush"];
         this.lastX = o["lastX"];
         this.lastY = o["lastY"];
         this.medianDt = o["medianDt"];
         this.lastDts = o["lastDts"];
 
-        this.elasticity = 0.1;
+        this.elasticity = 1;
     };
 
     behinstProto.tick = function ()
     {
         this.getLast5MedianDt();
-        this.pickupExternalImpulse();
-        var diff = { x: 0, y: 0 };
+        this.measureExternalImpuls();
 
-        if (this.stuckTracker.isStuck()) {window.console.log('stuck '+this.stuckTracker.unmovedTime)};
+        if (this.ticksToPush > 0)
+        {
+            this.inst.x += this.dx*this.medianDt;
+            this.inst.y += this.dy*this.medianDt;
+            this.ticksToPush -= 1;
+        }
+        else
+        {
+            this.dx = 0;
+            this.dy = 0;
+        }
+
         var diff = {
             x: this.inst.x - this.lastX,
             y: this.inst.y - this.lastY
         };
-        if (!movementNegligible(diff))  // save draw calls (and collisions) if nothing moves
+
+        if (!movementNegligible(diff) && this.enabled) // resolve collision & pickup counterspeed
         {
-            if (this.collisionsEnabled)
+            if (this.isMovingSlow(diff))
             {
-                this.inst.update_bbox();
-                if (this.isMovingSlow(diff))
-                {
-                    this.oneStepOrCollision(diff);
-                }
+                this.oneStepOrCollision(diff);
             }
+            // else should raycast. phew. --- or multisample !!
         }
 
-        window.console.log('measured movement - x: '+effectiveMove.x+', y: '+effectiveMove.y);
-        if (movementNegligible(effectiveMove))
-        {
-            this.stuckTracker.registerUnmoved(this.medianDt);
-        }
-        else
-        {
-            this.stuckTracker.registerFreed();
-        }
+        //if (movementNegligible(diff))
+        //{
+        //    this.stuckTracker.registerUnmoved(this.medianDt);
+        //}
+        //else
+        //{
+        //    this.stuckTracker.registerFreed();
+        //}
 
         this.lastX = this.inst.x;
         this.lastY = this.inst.y;
@@ -130,14 +145,19 @@ cr.behaviors.CollisionAware = function(runtime)
         this.medianDt = sample[2];
     }
 
-    behinstProto.pickupExternalImpulse = function ()
+    behinstProto.measureExternalImpuls = function ()
     {
-        if (this.lastX !== this.inst.x || this.lastY !== this.inst.y) // there are there other sources of movement
+        if (this.otherSourcesOfMovementExist())
         {
             var delta = this.getPositionDelta();
-            this.dx = (this.dx + delta.x/this.medianDt)/2;
-            this.dy = (this.dy + delta.y/this.medianDt)/2;
+            this.externDx = (this.externDx + delta.x/this.medianDt)/2;
+            this.externDy = (this.externDy + delta.y/this.medianDt)/2;
         }
+    }
+
+    behinstProto.otherSourcesOfMovementExist = function ()
+    {
+        return this.lastX !== this.inst.x || this.lastY !== this.inst.y
     }
 
     function movementNegligible (vector)
@@ -159,126 +179,17 @@ cr.behaviors.CollisionAware = function(runtime)
             && Math.abs(diff.y) < this.inst.bbox.height())
     }
 
-    behinstProto.moveAndOnCollisionDo = function (diff, collisionHandler)
+    behinstProto.oneStepOrCollision = function (diff)
     {
-        this.inst.x += diff.x;
-        this.inst.y += diff.y;
         this.inst.set_bbox_changed();
         var collobj = this.runtime.testOverlapSolid(this.inst);
         if (collobj)
         {
-            collisionHandler.call(this, diff, collobj);
-        }
-    }
-
-    behinstProto.oneStepOrCollision = function (diff)
-    {
-        this.moveAndOnCollisionDo(diff, function (diff, collobj) {
-            this.runtime.pushOutSolid(this.inst, -diff.x, -diff.y, Math.sqrt(diff.x*diff.x + diff.y*diff.y) + toleratedNumericError);
+            this.runtime.pushOutSolidNearest(this.inst, Math.sqrt(diff.x*diff.x + diff.y*diff.y) + toleratedNumericError);
             this.runtime.registerCollision(this.inst, collobj);
-            var pushbackX = this.inst.x - this.lastX - diff.x;
-            var pushbackY = this.inst.y - this.lastY - diff.y;
-            window.console.log('moveing back via collision: x: '+pushbackX+', y: '+pushbackY);
-            this.lastX = this.inst.x;
-            this.lastY = this.inst.y;
-            this.calculateBounceOffSpeed(collobj);
-            //var postCollisionDelta = this.getPositionDelta();
-            //if (!movementNegligible(postCollisionDelta))
-            //{
-            //    this.inst.set_bbox_changed();
-            //}
-        });
-    }
-
-    behinstProto.calculateBounceOffSpeed = function (c)
-    {
-        var constellation = this.detectConstellationWith(c);
-        this.flipOneSpeedComponentAwayFrom(c, constellation);       // try angled bounce first
-        this.applyFrictionToOtherSpeedComponent(constellation);
-
-        if (Math.abs(this.dx) > 0.7 || Math.abs(this.dy > 0.7))     // validate angled bounce is working
-        {
-            var speed = Math.sqrt(this.dx*this.dx + this.dy*this.dy);
-            var testMoveDistance = 2; // pixel // could depend on remaining distance not yet moved into collobj ??
-            //window.console.log('speed: '+speed);
-            var deltaX =  this.dx / speed * testMoveDistance;
-            var deltaY =  this.dy / speed * testMoveDistance;
-            window.console.log('test move: x: '+deltaX+', y: '+deltaY);
-            this.inst.x += deltaX;
-            this.inst.y += deltaY;
-            //this.inst.set_bbox_changed();
-            //var collobj = this.runtime.testOverlapSolid(this.inst);
-
-            if (false)  // angled bounce failed - switch to reflect
-            {
-                window.console.log('validation failed');
-                this.inst.x = this.lastX;
-                this.inst.y = this.lastY;
-                if (constellation === 'horizontal')
-                {
-                    this.dy = -this.elasticity*this.dy;
-                }
-                else
-                {
-                    this.dx = -this.elasticity*this.dx;
-                }
-            }
-        }
-    }
-
-    behinstProto.detectConstellationWith = function (c)
-    {
-        var toTheRight = this.inst.x > c.x;
-        var below = this.inst.y > c.y;
-        if (toTheRight === below)
-        {
-            var slopeDiagonalRightDown = (c.bquad.bry - c.bquad.midY())/(c.bquad.brx - c.bquad.midX());
-            var slopeToInstance = Math.abs(this.inst.y - c.bquad.midY())/(Math.abs(this.inst.x - c.bquad.midX()) + 1);
-            if ( slopeDiagonalRightDown < slopeToInstance)
-            {
-                return 'vertical';
-            }
-            else
-            {
-                return 'horizontal';
-            }
-        }
-        else
-        {
-            var slopeDiagonalRightUp = (c.bquad.try_ - c.bquad.midY())/(c.bquad.trx - c.bquad.midX());
-            var slopeToInstance = -Math.abs(this.inst.y - c.bquad.midY())/(Math.abs(this.inst.x - c.bquad.midX()) + 1);
-            if ( slopeDiagonalRightUp < slopeToInstance )
-            {
-                return 'horizontal';
-            }
-            else
-            {
-                return 'vertical';
-            }
-        }
-    }
-
-    behinstProto.flipOneSpeedComponentAwayFrom = function (c, direction)
-    {
-        if (direction === 'horizontal' && (this.inst.x - c.x)*this.dx < 0 )
-        {
-            this.dx = -this.elasticity*this.dx;
-        }
-        else if (direction === 'vertical' && (this.inst.y - c.y)*this.dy < 0 )
-        {
-            this.dy = -this.elasticity*this.dy;
-        }
-    }
-
-    behinstProto.applyFrictionToOtherSpeedComponent = function (direction)
-    {
-        if (direction === 'horizontal')
-        {
-            this.dy = 0.5*this.dy; // XXX 0.5 ===> this.friction
-        }
-        else if (direction === 'vertical')
-        {
-            this.dx = 0.5*this.dx;
+            this.dx += this.elasticity*(this.inst.x - (this.lastX + diff.x))/this.medianDt;
+            this.dy += this.elasticity*(this.inst.y - (this.lastY + diff.y))/this.medianDt;
+            this.ticksToPush += 1;
         }
     }
 
